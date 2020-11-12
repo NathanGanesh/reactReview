c2212365adf52aec285fe8ce1eb69c423b3b1478
nathan ganesh <489290@student.saxion.nl>
2020-11-12 10:09:35 +0100

    final push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import Review from './Review';
+import people from './data';
+
 function App() {
 	const [ count, setCount ] = React.useState(0);
+
+	const prevButton = () => {
+		if (count == 0) {
+			setCount(people.length - 1);
+		} else {
+			setCount(count - 1);
+		}
+	};
+
+	const handlePositiveChange = () => {
+		if (count == people.length - 1) {
+			setCount(0);
+		} else {
+			setCount(count + 1);
+		}
+	};
+
+	const supriseMe = () => {
+		const peoplelength = people.length;
+		const randomNumber = Math.floor(Math.random() * peoplelength);
+		// console.log();
+		setCount(randomNumber);
+	};
 
 	return (
 		<div>
 			<h2>Our Reviews</h2>
 			<div className="underline" />
-			<Review count={count} />
+			<Review
+				count={people[count]}
+				prevButton={prevButton}
+				nextButton={handlePositiveChange}
+				randomButton={supriseMe}
+			/>
 		</div>
 	);
 }
--- src/Review.js
+++ src/Review.js
@@ -2,9 +2,33 @@ import React, { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
-const Review = ({ count }) => {
-	console.log(people[count]);
-	return <div />;
+const Review = ({ count, prevButton, nextButton, randomButton }) => {
+	const { image, name, job, text } = count;
+
+	return (
+		<div className="main-review">
+			<div className="main-quote-box">
+				<FaQuoteRight className="main-quote" />
+			</div>
+			<div className="main-image">
+				<img src={image} alt={name} />
+			</div>
+			<h5 className="review-name">{name}</h5>
+			<h5 className="review-job">{job}</h5>
+			<h6 className="review-description">{text}</h6>
+			<div className="chevron">
+				<button className="prev-btn" onClick={prevButton}>
+					<FaChevronLeft />
+				</button>
+				<button className="next-btn" onClick={nextButton}>
+					<FaChevronRight />
+				</button>
+			</div>
+			<button className="suprise-me" onClick={randomButton}>
+				Suprise Me
+			</button>
+		</div>
+	);
 };
 
 export default Review;
